fa77bb0406e4839ad0dfcf0ee43aa3433c31072e
samarthpathak62-cmyk <samarthpathak62@gmail.com>
2026-05-24 20:59:34 +0530

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,27 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+}
+rootProject.name = "Fold Craft Launcher"
+include(":FCL")
+include(":FCLCore")
+include(":FCLauncher")
+include(":FCLLibrary")
+include(":LWJGL-Pojav")
+include(":Terracotta")
+include(":ZipFileSystem")
+//include(":NG-GL4ES")
